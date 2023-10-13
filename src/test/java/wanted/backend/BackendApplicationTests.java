f744626ac40671186117300918d025984b9e354a
kkzz0001 <kkzz0001@gmail.com>
2023-10-13 17:38:18 +0900

    SpringBoot Project 생성 Repository 생성

---
+++ src/test/java/wanted/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
